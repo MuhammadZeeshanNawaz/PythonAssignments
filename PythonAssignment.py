c3909a20125ac09574cdd038ac86e7636f1a948b
Muhammad Zeeshan Nawaz <zeeshann463@gmail.com>
2019-11-04 02:05:41 +0500

    First Python Assignment

---
+++ PythonAssignment.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Question no 01
+
+
+print ("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star \n\tHow I wonder what you are ")
+
+
+# Question no 02
+
+
+print ("My Python Version is:")
+import sys
+print (sys.version)
+
+
+# Question no 03
+
+
+import time
+print ("Current date and time :")
+print(time.ctime())
+
+
+# Question no 04
+
+
+pie = 3.142
+print ("Enter the radius of a circle")
+radius = int(input())
+area = pie * (radius*radius)
+print (area)
+
+
+# Question no 05
+
+
+print ("Enter Your Name")
+fname = str(input())
+lname = str(input())
+
+print("Reversed name: ", lname, fname )
+
+
+# Question no 06
+
+
+print ("Enter Your First Number")
+a = int(input())
+print ("Enter Your Second Number")
+b = int(input())
+result = a + b
+print ("Addition of your numbers is",result)
+
