1e05d70b144470d81c82e52eb70861b1ab53e305
Muhammad Zeeshan Nawaz <zeeshann463@gmail.com>
2019-11-15 18:10:42 +0500

    Python Second Assignment

---
+++ Assignment no.02.py
@@ -0,0 +1,52 @@
+
+-------------------------
+#question no 2:
+
+#Zeeshan Rizwan
+number = int(input("Enter Number:"))
+if number%2 == 0:
+    print("Number ",number," is Even")
+else:
+    print("Number ",number," is Odd")
+
+	
+------------------
+#question  no 3:
+
+#Zeeshan Rizwan
+
+n= [1,3,5,8,9,10]
+print(len(n))
+
+--------------------------
+#question no 4:
+
+#Zeeshan Rizwan
+
+num= [1,2,3]
+a=n[0]+n[1]+n[2]
+print(a)
+
+---------------
+#question no 5:
+
+#Zeeshan Rizwan
+a=[10,5,9,16]
+max_num=0
+for i in range(0,len(a)):
+    if max_num<a[i]:
+        max_num=a[i]
+        
+        
+print(max_num)
+
+----------------
+#question no 6:
+
+#Zeeshan Rizwan
+arr = [2,4,5,6,1,4,2,25,10,7,84]
+
+for i in range(0,len(arr)):
+    if arr[i] < 5:
+       print(arr[i])
+
